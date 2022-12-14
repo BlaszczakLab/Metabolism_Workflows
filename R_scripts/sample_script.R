d6828d201ad5a71e6d516622980c0a0617b228b3
Heili Lowman <heili.lowman@gmail.com>
2022-12-14 11:47:28 -0500

    Creating R scripts folder with sample script.

---
+++ R_scripts/sample_script.R
@@ -0,0 +1,29 @@
+#### Sample R Script
+#### Creation Date: December 14, 2022
+#### Contributors: Heili Lowman
+
+# This is a sample script to begin the record in the "R_scripts" folder.
+
+# All scripts in the Metabolism workflow should be well annotated, and
+# follow the same general workflow. In addition, all file paths must
+# use universal structuring (e.g., using the `here` package) so that code
+# is able to be used across multiple peoples' computers.
+
+# Load packages.
+library(tidyverse)
+library(lubridate)
+library(here)
+
+# Load data.
+df <- read_csv("data_raw/nwis_11462500_input.csv")
+
+# Perform necessary operations.
+
+df <- df %>%
+  mutate(Date = ymd_hms(solar.time))
+
+(fig1 <- ggplot(df, aes(x = Date, y = DO.obs)) +
+  geom_line(color = "aquamarine") +
+  theme_bw())
+
+# End of script.
